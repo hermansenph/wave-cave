74456b781f791cb35d52bb6083ae5f3f1ae378d5
hermansenph <hermansenph@gmail.com>
2017-11-06 13:14:35 -0800

    Use server/index.js and create-app.js to serve static middleware #1

---
+++ server/index.js
@@ -0,0 +1,8 @@
+require('dotenv/config')
+const { createApp } = require('./create-app')
+
+const app = createApp()
+
+app.listen(process.env.PORT, () => {
+  console.log('Listening on ' + process.env.PORT)
+})

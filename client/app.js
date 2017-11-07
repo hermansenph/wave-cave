26e3b47c09b31566a737fb08b634b0b5db3da5cb
hermansenph <hermansenph@gmail.com>
2017-11-06 16:19:13 -0800

    Add updateArt method that displays selected image #1

--- client/app.js
+++ client/app.js
@@ -3,9 +3,28 @@ import UploadForm from './upload-form'
 
 export default class App extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = { image: './images/image-placeholder.png' }
+    this.updateArt = this.updateArt.bind(this)
+  }
+
+  updateArt(event) {
+    const reader = new FileReader()
+
+    reader.addEventListener('load', () => {
+      this.setState({image: reader.result})
+    })
+
+    if (event.target.files[0]) {
+      reader.readAsDataURL(event.target.files[0])
+    }
+    console.log(event.target.files[0])
+  }
+
   render() {
     return (
-      <UploadForm/>
+      <UploadForm handleChange={ this.updateArt } updateArt={ this.updateArt } image={ this.state.image }/>
     )
   }
 }
--- client/upload-form.js
+++ client/upload-form.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
-export default function UploadForm() {
+export default function UploadForm({ image, updateArt }) {
   return (
     <div className="container">
       <div className="card">
         <form>
           <div className="row row-grid">
             <div className="mx-auto">
+              <img src={ image } id="upload-art" className="img-thumbnail .img-fluid"/>
+            </div>
+          </div>
+          <div className="row row-grid">
+            <div className="mx-auto">
               <label className="btn btn-outline-primary">
-                Select Image <input type="file" name="image" id="image-upload" accept="image/*" hidden/>
+                Select Image <UploadImage updateArt={ updateArt }/>
               </label>
             </div>
           </div>
@@ -37,5 +42,11 @@ export default function UploadForm() {
         </form>
       </div>
     </div>
+  )
+}
+
+function UploadImage({ updateArt }) {
+  return (
+    <input onChange={ updateArt } type="file" name="image" id="image-upload" accept="image/*" hidden/>
   )
 }

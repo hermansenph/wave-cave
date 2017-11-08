d0af9999413746bcde575b5808b1b7dd8bff3184
hermansenph <hermansenph@gmail.com>
2017-11-08 13:39:23 -0800

    Create POST route handler that uploads files to google cloud storage server #1

--- server/create-app.js
+++ server/create-app.js
@@ -3,19 +3,40 @@ const path = require('path')
 const multer = require('multer')
 const mime = require('mime-types')
 const crypto = require('crypto')
+const fs = require('fs')
+const Storage = require('@google-cloud/storage')
+require('dotenv').config()
+
+const storageGoogle = new Storage()
 
 const storage = multer.diskStorage({
   destination: 'server/uploads/',
-  filename: (rew, file, cb) => {
+  filename: (req, file, cb) => {
     crypto.pseudoRandomBytes(16, (err, raw) => {
       if (err) return cb(err)
 
+      const extension = mime.extension(file.mimetype)
+      const fileName = {fileName: raw.toString('hex') + '.' + extension}
+
+      req[extension] = fileName
       cb(null, raw.toString('hex') + '.' + mime.extension(file.mimetype))
     })
   }
 })
 
 const upload = multer({ storage: storage })
+
+const googleStorage = (file) => {
+  storageGoogle
+    .bucket('wave-cave')
+    .upload('server/uploads/' + file)
+    .catch(err => {
+      console.error('ERROR:', err)
+    })
+    .then(() => {
+      fs.unlink('server/uploads/' + file)
+    })
+}
 
 function createApp() {
 
@@ -24,11 +45,20 @@ function createApp() {
   app
     .use(express.static(path.join(__dirname, 'public')))
     .post('/upload', upload.fields([
+
       {name: 'image-file'},
       {name: 'audio-file'}
+
     ]), (req, res) => {
-      console.log(req.body)
-      console.log(req.files)
+
+      const uploadData = {}
+      Object.assign(uploadData, req.body)
+      uploadData.image = req.jpeg.fileName
+      uploadData.audio = req.mp3.fileName
+
+      googleStorage(uploadData.image)
+      googleStorage(uploadData.audio)
+
     })
   return app
 }

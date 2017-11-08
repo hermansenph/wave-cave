ad9b5b0d52903c282e6695f87b2cae60136491bd
hermansenph <hermansenph@gmail.com>
2017-11-08 10:52:46 -0800

    Implement multer middleware to receive files #1

--- server/create-app.js
+++ server/create-app.js
@@ -1,5 +1,21 @@
 const express = require('express')
 const path = require('path')
+const multer = require('multer')
+const mime = require('mime-types')
+const crypto = require('crypto')
+
+const storage = multer.diskStorage({
+  destination: 'server/uploads/',
+  filename: (rew, file, cb) => {
+    crypto.pseudoRandomBytes(16, (err, raw) => {
+      if (err) return cb(err)
+
+      cb(null, raw.toString('hex') + '.' + mime.extension(file.mimetype))
+    })
+  }
+})
+
+const upload = multer({ storage: storage })
 
 function createApp() {
 
@@ -7,7 +23,13 @@ function createApp() {
 
   app
     .use(express.static(path.join(__dirname, 'public')))
-
+    .post('/upload', upload.fields([
+      {name: 'image-file'},
+      {name: 'audio-file'}
+    ]), (req, res) => {
+      console.log(req.body)
+      console.log(req.files)
+    })
   return app
 }
 

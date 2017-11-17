04e638590c21956c1303018e7a0f5a3ea20c453d
hermansenph <hermansenph@gmail.com>
2017-11-16 20:40:06 -0800

    Create method to change both player play button and song list item play button when either is clicked #2

--- client/player-app.js
+++ client/player-app.js
@@ -5,62 +5,64 @@ export default class PlayerApp extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      songList: [],
-      song: {
-        title: '',
-        artist: '',
-        audio: '',
-        image: ''
-      },
-      playPause: './images/play-button.svg',
+      playPause: '',
       duration: 0,
-      audio: ''
+      title: '',
+      artist: '',
+      song: '',
+      id: ''
     }
     this.playAudio = this.playAudio.bind(this)
     this.setVolume = this.setVolume.bind(this)
     this.scrubTime = this.scrubTime.bind(this)
-    this.setAudio = this.setAudio.bind(this)
-    this.renderSong = this.renderSong.bind(this)
   }
 
   async componentDidMount() {
-
-    const songsResponse = await fetch('http://localhost:3000/songs')
-    const songs = await songsResponse.json()
-    this.setState({
-      songList: songs
-    })
 
     setInterval(() => {
       this.time.value = this.audio.currentTime
     }, 15)
 
-  }
-
-  async setAudio(song) {
-    await this.setState({
-      audio: song
-    })
-    this.audio.src = this.state.audio
     this.audio.addEventListener('loadedmetadata', () => {
-      this.state.duration = this.audio.duration
-      this.playAudio()
+      this.setState({duration: this.audio.duration})
+      this.audio.play()
     })
+
   }
 
-  async playAudio() {
+  async componentWillReceiveProps(nextProps) {
 
-    if (this.state.playPause === './images/play-button.svg') {
-      await this.setState({
-        playPause: './images/pause-button.png',
-        duration: this.audio.duration
+    if (this.props.song.audio !== nextProps.song.audio) {
+      this.audio.pause()
+      if (this.props.song.audio !== undefined) {
+        fetch('/songs/' + this.props.song.audio, {method: 'delete'})
+      }
+      await fetch('/songs-download/' + nextProps.song.audio)
+      this.setState({
+        title: nextProps.song.title,
+        artist: nextProps.song.artist,
+        song: nextProps.song.audio,
+        playPause: nextProps.song.playPause,
+        id: nextProps.song.id
       })
+    }
+
+    else {
+      this.setState({
+        playPause: nextProps.song.playPause
+      })
+      this.playAudio()
+    }
+
+  }
+
+  playAudio() {
+    if (this.state.playPause === './images/play-button.png') {
       this.audio.play()
     }
 
     else {
       this.audio.pause()
-      this.setState({playPause: './images/play-button.svg'})
     }
 
   }
@@ -73,77 +75,53 @@ export default class PlayerApp extends React.Component {
     this.audio.currentTime = this.time.value
   }
 
-  renderSong({ artist, audio, image, title }, index) {
-    const id = 'song-' + index
-    const audioURL = './songs/' + audio
-    const imageURL = './songs/' + image
-
-    return <div className="song">
-      <input
-        className="song-list-play"
-        id={ id }
-        onClick={ () => {
-
-          if (this.audio.src !== 'http://localhost:3000/songs/' + audio) {
-            this.setAudio(audioURL)
-          }
-
-          else this.playAudio()
-
-        }}
-        type="image"
-        src={ this.state.playPause }
-      >
-      </input>
-      <img className="song-image" src={ imageURL }/>
-      <p> Title: { title }<br/> Artist: { artist }</p>
-    </div>
-  }
-
   render() {
     return (
-      <div id="songs">
-        <div id="song-list">
-          {this.state.songList.map(this.renderSong)}
+      <div id="song-player">
+        <div id="song-player-play-div">
+          <input
+            id="song-player-play"
+            onClick={ () => {
+              this.props.playPause(this.state.id)
+              this.playAudio()
+            }}
+            type="image"
+            src={ this.state.playPause }
+          >
+          </input>
         </div>
-        <div id="audio-player">
-          <div id="song-player-play-div">
-            <input
-              id="song-player-play"
-              onClick={ this.playAudio }
-              type="image"
-              src={ this.state.playPause }
-            >
-            </input>
+        <div id="song-player-time-div">
+          <div>
+            <p className="player-song-info player-song-title">{this.state.title}</p>
+            <p className="player-song-info">{' - '}</p>
+            <p className="player-song-info player-song-artist">{this.state.artist}</p>
           </div>
-          <div id="song-player-time-div">
-            <input
-              id="song-player-time"
-              onChange={ this.scrubTime }
-              type="range" min="0" max={ this.state.duration } step="any"
-              ref={time => {
-                this.time = time
-              }}
-            />
-          </div>
-          <div id="song-player-volume-div">
-            <input
-              id="song-player-volume"
-              onChange={ this.setVolume }
-              type="range" min="0" max="1" step="any"
-              ref={volume => {
-                this.volume = volume
-              }}
-            />
-          </div>
-          <audio
-            src={ this.state.audio }
-            controls=""
-            ref={audio => {
-              this.audio = audio
+          <input
+            id="song-player-time"
+            onChange={ this.scrubTime }
+            type="range" min="0" max={ this.state.duration } step="any"
+            ref={time => {
+              this.time = time
             }}
           />
         </div>
+        <div id="song-player-volume-div">
+          <input
+            id="song-player-volume"
+            onChange={ this.setVolume }
+            type="range" min="0" max="1" step="any"
+            ref={volume => {
+              this.volume = volume
+            }}
+          />
+        </div>
+        <audio
+          src={ '/songs/' + this.state.song }
+          controls=""
+          ref={audio => {
+            this.audio = audio
+          }}
+        />
       </div>
     )
   }

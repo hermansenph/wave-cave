ca55d6891e56ad86bcb13fc798393a2251a3d2b2
hermansenph <hermansenph@gmail.com>
2017-11-16 20:46:42 -0800

    Added a find method that returns a songList #2

--- server/uploads-gateway.js
+++ server/uploads-gateway.js
@@ -1,7 +1,14 @@
 module.exports = function uploadsGateway(collection) {
   return {
+
     async create(uploadData) {
       collection.insertOne(uploadData)
+    },
+
+    async find() {
+      const songList = await collection.find({}).toArray()
+      return songList
     }
+
   }
 }
